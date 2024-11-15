5555d5740d3f9793b6d819968c750547b1112458
Florian Moser <git@famoser.ch>
2024-11-15 09:10:12 +0100

    feat: Improve ancestry reconstruction

--- src/rule_explainer.rs
+++ src/rule_explainer.rs
@@ -12,12 +12,19 @@ impl QueryExplainer {
 
         let mut skip_queue_entries = 0;
 
-        for iteration in iterations.iter().rev() {
+        for i in (0..iterations.len()).rev() {
+            let iteration = &iterations[i];
             let new_queue_entries = iteration.new_queue_entries.len();
 
             if skip_queue_entries < new_queue_entries || ancestry.is_empty() {
                 ancestry.push(iteration.clone());
-                skip_queue_entries = iteration.progress.in_queue;
+                if i == 0 {
+                    break;
+                }
+
+                // need to take from previous iteration, because in case of deriving bad, the queue is emptied
+                skip_queue_entries = iterations[i - 1].progress.in_queue - 1;
+                continue;
             }
 
             skip_queue_entries -= new_queue_entries
